3d3ed061256c1908664f3d20dd8fe67bd765e9a1
H. Juan David Romero P <hermes.romero@unipamplona.edu.co>
2020-12-20 10:47:38 -0500

    Update db_connection

--- db/db_connection.py
+++ db/db_connection.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Creando Motor y Conexion con la Base de Datos
-SQLALCHEMY_DATABASE_URL = "postgres://sfodbpojxombnd:6a2295d8561d7c1abe2c1aa6d400be2135c05d255e5a37620ad3d8f0d26a5634@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d56asiq1bveijh"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ds4biz23@localhost:5432/MISION_TIC"
+SQLALCHEMY_DATABASE_URL = "postgres://sfodbpojxombnd:@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d56asiq1bveijh"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:@localhost:5432/MISION_TIC"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Creacion de la Sesión
